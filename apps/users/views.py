16a5ce65af619a1efb001c5fd7bf59dc5e397870
whisnos <whisnos@163.com>
2019-12-17 22:42:01 +0800

    dev

--- apps/users/views.py
+++ apps/users/views.py
@@ -38,29 +38,39 @@ def test(request):
     guishudi = request.GET.get('arr[guishudi]', '')  # 归属地
     fy = request.GET.get('arr[fy]', '')  # 分页
     print('品牌', pp)
-    filter = Q()
+    yls1 = Q()
     if pp:
         print(22)
-        filter = Q(car_brand=pp)
-
+        pp1 = Q(car_brand=pp)
+    else:
+        pp1 = Q()
     if yls:
         yls = yls.replace('万', '')
         yls = yls.split('-')
         l_price = yls[0]
         g_price = yls[1]
         print('yls', l_price, g_price)
-        if not pp:
-            filter = Q(car_price__gte=int(l_price)) & Q(car_price__lte=int(g_price))
-        else:
-            filter = Q(car_brand=pp) & Q(car_price__gte=int(l_price)) & Q(car_price__lte=int(g_price))
+        # if not pp:
+        #     yls1 = Q(car_price__gte=int(l_price)) & Q(car_price__lte=int(g_price))
+        # else:
+        #     yls1 = pp1 & Q(car_price__gte=int(l_price)) & Q(car_price__lte=int(g_price))
+        yls1 = pp1 & Q(car_price__gte=int(l_price)) & Q(car_price__lte=int(g_price))
+    else:
+        yls1 = pp1
     if zz:
-        print(22)
-        filter = Q(car_brand=pp)
-    if pp:
-        print(22)
-        filter = Q(car_brand=pp)
-    print('filter', filter)
-    all_cars = CarInfo.objects.filter(filter)
+        zz = zz.split('-')
+        l_age = zz[0]
+        g_age = zz[1]
+        print('zz', l_age, g_age)
+        yls1 = Q(car_age__gte=int(l_age)) & Q(car_age__lte=int(g_age)) & yls1
+    else:
+        yls1 = yls1
+    if guishudi:
+
+        yls1 = Q(area=int(guishudi)) & yls1
+    else:
+        yls1 =  yls1
+    all_cars = CarInfo.objects.filter(yls1)
     print('all_cars', all_cars)
     if not all_cars:
         all_cars = ''
